a88f1b945ad9194802e0787fa253932c34c9543e
JulR <mi@jrei.ch>
2017-04-17 21:39:34 +0200

    Initial commit

--- a.sh
+++
@@ -1,7 +0,0 @@
-cat .gitmodules | while read line; do
-  case $line in
-    path*) path=${line#*= };;
-    url*) url=${line#*= };;
-    branch*) branch=${line#*= }; git clone $url -b $branch $path;;
-  esac
-done
